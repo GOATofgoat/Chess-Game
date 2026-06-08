98cd2ee61d7f192911ed44548badcc6140bf3f44
Louis Urbina <appleurbina19@gmail.com>
2026-06-08 18:27:52 -0400

    Fixed background path

--- constants.py
+++ constants.py
@@ -8,8 +8,8 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 # Asset paths
-BACKGROUND_IMG_PATH = "images/background.png"
-CHESSBOARD_IMG_PATH = "images/chessboard.png"
+BACKGROUND_IMG_PATH = "assets/background.png"
+CHESSBOARD_IMG_PATH = "assets/chessboard.png"
 
 # Piece sizing
 PIECE_IMAGE_SCALE = (40, 50)  # (width, height) in pixels
